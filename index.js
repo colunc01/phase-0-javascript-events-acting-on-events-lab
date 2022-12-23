77aee829bc9c5aca94bbd16548bbc6ac0cfe35a3
Cesar Colunga <cesara.colunga@gmail.com>
2022-12-23 15:27:20 -0700

    commit tree ae89c3ad475f400f336f9f5b116139178fc05e99

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (360 > left) {
+        dodger.style.left = `${left + 1}px`;
+    }
+
+    /******************
+     * var left = dodger.style.left
+    left = parseInt(left)
+
+    moveDodgerRight()
+
+    var newPosition = dodger.style.left
+    newPosition = parseInt(newPosition)
+
+    expect(newPosition).to.be.above(left)
+     */ 
+
+}
